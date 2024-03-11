fa7b6e01134a4073c4b415cc331a514d1f5f76af
Tomáš Peniak <71640446+pennyb0i@users.noreply.github.com>
2024-03-11 18:59:43 +0100

    Ingredient amount to decimal

--- RecipeService/Core/Models/BindingModels/RecipeIngredientPutBindingModel.cs
+++ RecipeService/Core/Models/BindingModels/RecipeIngredientPutBindingModel.cs
@@ -3,6 +3,6 @@ namespace RecipeService.Core.Models.BindingModels;
 
 public class RecipeIngredientPutBindingModel {
     public Guid IngredientId { get; set; }
-    public int Amount { get; set; }
+    public decimal Amount { get; set; }
     public MeasurementUnits Unit { get; set; }
 }
